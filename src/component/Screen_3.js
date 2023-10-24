faf8b8054be296e7de599d6f22ccb18c4c439808
PhuongCuong <20078421.cuong@student.iuh.edu.vn>
2023-10-24 14:00:39 +0700

    finish giao dien screen 3 cau 2

--- src/component/Screen_3.js
+++ src/component/Screen_3.js
@@ -2,48 +2,38 @@ import {Image, Pressable, Text, TextInput, View} from "react-native"
 
 const Screen_3 = () =>{
     return(
-        <View style={{flex:7,alignItems:"center"}}>
-            <View style={{flex:3,justifyContent:"center",alignItems:"center"}}>
-                <Image style={{width:80,height:80}} source={require('../../assets/DATA/Image 19.png')}/>
-                <Text style={{fontFamily:"Roboto",fontSize:30,fontWeight:"700",color:"black"}}>
-                    Nice to see you!
-                </Text>
-                <Text style={{fontFamily:"Roboto",fontSize:17,fontWeight:"500",color:"gray"}}>
-                    Create your account
-                </Text>
-
+        <View style={{flex:10}}>
+            <View style={{flex:3}}>
+            <Image style={{height:"100%",width:"100%"}} source={require("../../assets/DATA/Image 20.png")}/>
             </View>
-            <View style={{flex:4,width:350,alignItems:"center"}}>
-                <View style={{width:"100%",height:45,borderWidth:1,borderRadius:10,flexDirection:"row",alignItems:"center",marginBottom:10}}>
-                    <Image style={{marginLeft:5,width:20,height:20}} source={require("../../assets/DATA/codicon_account.png")}/>
-                    <TextInput style={{width:300,height:43,marginLeft:5,outline:"none",fontFamily:"Roboto",fontSize:17,fontWeight:"500"}}
-                    placeholder="Enter your user name"
-                    />
+            <View style={{flex:7,borderRadius:20}}>
+                <View style={{flex:3,justifyContent:"center"}}>
+                    <Text style={{fontFamily:"Roboto",fontSize:30,fontWeight:"700",color:"black",margin:30}}>
+                        Welcome!
+                    </Text>
                 </View>
-                <View style={{width:"100%",height:45,borderWidth:1,borderRadius:10,flexDirection:"row",alignItems:"center",marginBottom:10,marginTop:10}}>
+                <View style={{flex:3,alignItems:"center"}}>
+                <View style={{width:350,height:45,borderWidth:1,borderRadius:10,flexDirection:"row",alignItems:"center",marginBottom:10,marginTop:10}}>
                     <Image style={{marginLeft:5,width:20,height:20}} source={require("../../assets/DATA/Vector.png")}/>
                     <TextInput style={{width:300,height:43,marginLeft:5,outline:"none",fontFamily:"Roboto",fontSize:17,fontWeight:"500"}}
-                    placeholder="Enter your email address"
+                    placeholder="Enter email"
                     />
                 </View>
-                <View style={{width:"100%",height:45,borderWidth:1,borderRadius:10,flexDirection:"row",alignItems:"center",marginBottom:10,marginTop:10}}>
+                <View style={{width:350,height:45,borderWidth:1,borderRadius:10,flexDirection:"row",alignItems:"center",marginBottom:10,marginTop:10}}>
                     <Image style={{marginLeft:5,width:20,height:20}} source={require("../../assets/DATA/lock.png")}/>
                     <TextInput secureTextEntry={true} style={{width:290,height:43,marginLeft:5,outline:"none",fontFamily:"Roboto",fontSize:17,fontWeight:"500"}}
-                    placeholder="Enter your password"
+                    placeholder="Enter password"
                     />
                     <Image style={{marginRight:5,width:20,height:20}} source={require("../../assets/DATA/eye.png")}/>
+                </View>   
                 </View>
-                <View style={{width:"100%",height:45,flexDirection:"row",alignItems:"center",marginBottom:10,marginTop:10}}>
-                    <Text style={{fontFamily:"Roboto",fontSize:17,color:"gray"}}>
-                        I agree with 
-                        <Text style={{fontFamily:"Roboto",fontSize:17,color:"blue"}}> Terms & Conditions</Text>
-                    </Text>
+                <View style={{flex:1,alignItems:"center"}}>
+                    <Pressable style={{width:350,height:45,backgroundColor:"blue",borderRadius:20,justifyContent:"center",alignItems:"center"}}>
+                        <Text style={{fontFamily:"Roboto",fontSize:20,fontWeight:"700",color:"white"}}>
+                            Login
+                        </Text>
+                    </Pressable>
                 </View>
-                <Pressable style={{width:"100%",height:45,flexDirection:"row",alignItems:"center",marginBottom:10,marginTop:10,justifyContent:"center",backgroundColor:"blue",borderRadius:10}}>
-                    <Text style={{fontFamily:"Roboto",fontSize:20,fontWeight:"700",color:"white"}}>
-                        Continue
-                    </Text>
-                </Pressable>
             </View>
         </View>
     )
